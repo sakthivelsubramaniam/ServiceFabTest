f8c65706a8af49a4154fb040c8bc5f6cde58009f
sakthivel <sakthivelcs@hotmail.com>
2022-02-08 10:28:10 +0530

    added fp-ts

--- fp-ts/datastruct.ts
+++ fp-ts/datastruct.ts
@@ -50,4 +50,25 @@ const v7 = O.fromNullable(rightEither); // option.Some("value")
 const v8 = O.none;
 const v9 = O.of("value");
 
-/// 
+// Either
+console.log("building eitherA");
+console.log("### fromNullable ###");
+const lv1 = E.fromNullable("default value for either")(null); // same as E.left("default value of either");
+const rv1 = E.fromNullable("default value")("some value"); // same as E.right("some value");
+
+console.log("### from predicate ###");
+// const isEven = (number ) => number %2 === 0 ;
+
+type EvenNumber = number;
+const isEvenTypeGaurd = (num:number):num is EvenNumber => num % 2 ===0;
+const eitherBuilder = E.fromPredicate(
+	isEven,
+	(number) => `${number} is an odd number`
+);
+// Here when you use eitherBuilder you get something with the type Either<string, number>
+const leftValue = eitherBuilder(3); // Either.left('3 is an odd number')
+const rightValue = eitherBuilder(4); // Either.right(4)
+
+
+
+//
